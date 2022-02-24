af97af456395842c1eb8f779a91cdcaceefb2f1d
matthieuvrcmr <90846875+matthieuvrcmr@users.noreply.github.com>
2022-02-24 17:44:56 +0100

    Update Adventices.py

--- Comptage Adventice/Adventices.py
+++ Comptage Adventice/Adventices.py
@@ -10,24 +10,32 @@ import numpy as np
 from skimage.filters import threshold_yen
 
 
-PATH = "photos_crop/"
-IMG_NAMES = ["DJI_00"+str(nb)+"_crop.JPG" for nb in np.arange(50,61)]
-
-# Calcul de la surface d'adventices en appliquant un filtre YEN
-def surface_adventices_yen(img_name):
-    rgb_image = cv.imread(PATH+img_name)
+# Calcul de la surface d'adventices en appliquant un filtre YEN et affiche la photo il faut renseigner le chemin vers l'image
+def surface_adventices_yen(img_path):
+    rgb_image = cv.imread(img_path)
     green_channel = rgb_image[:,:,1]
     yen_th = threshold_yen(green_channel)
     ret1,th1 = cv.threshold(green_channel,yen_th,255,cv.THRESH_BINARY)
+    plt.imshow(th1)
+    plt.show()
     return th1, 100*np.sum(th1==0)/th1.size
 
-# Calcul de la surface d'adventices en appliquant un filtre OTSU
-def surface_adventices_otsu(img_name):
-    rgb_image = cv.imread(PATH+img_name)
+# Calcul de la surface d'adventices en appliquant un filtre OTSU et affiche la photo il faut renseigner le chemin vers l'image
+def surface_adventices_otsu(img_path):
+    rgb_image = cv.imread(img_path)
     green_channel = rgb_image[:,:,1]
-    ret2,th2 = cv.threshold(green_channel,0,255,cv.THRESH_BINARY+cv.THRESH_OTSU)    
+    ret2,th2 = cv.threshold(green_channel,0,255,cv.THRESH_BINARY+cv.THRESH_OTSU)   
+    plt.imshow(th2)
+    plt.show()
     return th2, 100*np.sum(th2==0)/th2.size
 
+    
+    
+
+"""Renseigner le chemin vers les images, ici on utilise les images DJI_0050 à DJI_0060"""
+PATH = "photos_crop/"
+IMG_NAMES = ["DJI_00"+str(nb)+"_crop.JPG" for nb in np.arange(50,61)]
+    
 # Auto Thresold Yen
 for IMG_NAME in IMG_NAMES:
     th, surf_adv = surface_adventices_yen(IMG_NAME)
@@ -54,24 +62,6 @@ fig.add_subplot(1,2,2)
 plt.imshow(th2)
 plt.title('DJI_0057 : filtre Otsu sur canal vert')
 plt.savefig('Adventices_Filtre_Yen_Otsu')
-
-------------------------------------------------------------------------------
-# Comptage de trous de corbeaux
-PATH_CROW : "DJI_0084_crows.JPG"
-rgb_image = cv.imread("DJI_0084_crows.JPG")
-green_channel = rgb_image[:,:,1]
-ret2,th2 = cv.threshold(green_channel,0,255,cv.THRESH_BINARY+cv.THRESH_TRIANGLE)  
-
-
-#affichage des images
-fig = plt.figure(figsize=(10, 7))
-fig.add_subplot(1,2,1)
-plt.imshow(rgb_image)
-plt.title('Photo originale :DJI_0084')
-fig.add_subplot(1,2,2)
-plt.imshow(th2)
-plt.title('Application du filtre triangle sur canal vert')
-plt.savefig('Trous_corbeau_Filtre_Triangle')
 
 
 
